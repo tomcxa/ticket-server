eb66676685d16d9d8b24475e05320fe6fe61f8fa
Anton Golikov <tomcxa@yandex.ru>
2020-05-03 14:15:20 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const app = new Koa();
+
+app.use(koaBody({
+    text: true,
+    urlencoded: true,
+    multipart: true,
+    json: true,
+}));
+
+app.use(async (ctx, next) => {
+    const origin = ctx.request.get('Origin');
+    if (!origin) {
+        return await next();
+    }
+    const headers = { 'Access-Control-Allow-Origin': '*', };
+    if (ctx.request.method !== 'OPTIONS') {
+        ctx.response.set({ ...headers });
+        try {
+            return await next();
+        } catch (e) {
+            e.headers = { ...e.headers, ...headers };
+            throw e;
+        }
+    }
+    if (ctx.request.get('Access-Control-Request-Method')) {
+        ctx.response.set({
+            ...headers,
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+        });
+        if (ctx.request.get('Access-Control-Request-Headers')) {
+            ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
+        }
+        ctx.response.status = 204; // No content
+    }
+});
+
+app.use(async (ctx) => {
+    console.log(ctx.request.querystring);
+    console.log(ctx.request.body);
+    ctx.response.body = 'server response';
+});
+
+
+const server = http.createServer(app.callback()).listen(7070);
